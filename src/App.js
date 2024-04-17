8251a4cdc13ee16322547f03f270cc4e21548fdd
MudasserAhmed <farhan.ali>
2024-04-17 11:15:04 +0500

    api integrate select comp

--- src/App.js
+++ src/App.js
@@ -2,45 +2,48 @@ import logo from "./logo.svg";
 import "./App.css";
 import BasicInput from "./component/BasicInput";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BasicSelect from "./component/BasicSelect";
 import BasicCheckbox from "./component/BasicCheckbox";
+import axios from "axios";
 function App() {
-  const [inputValue, setInputValue] = useState('');
-  const [selectedOption , setSelectedOption] = useState([]);
-  const [checked,setChecked] = useState(false)
-  const onCheckChange = (e)=>{
-    console.log(e.target.checked)
-    setChecked(e.target.checked)
-  }
-  const options = [
-    { value: 'option1', label: 'Option 1' },
-    { value: 'option2', label: 'Option 2' },
-    { value: 'option3', label: 'Option 3' },
-    { value: 'option4', label: 'Option 4' },
-    { value: 'option4', label: 'Option 4' },
-    { value: 'option4', label: 'Option 4' },
-    { value: 'option4', label: 'Option 4' },
-    { value: 'option4', label: 'Option 4' },
-    { value: 'option4', label: 'Option 4' },
-    { value: 'option4', label: 'Option 4' },
-  ];
-
-
-   const handleSelectChange = (e,index)=>{
-      console.log(e.target.value)
+  const [users,setUsers] = useState([])
+   useEffect(()=>{
+    getUsers()
+   },[])
+   const getUsers = async ()=>{
+    try {
+      const response = await axios.get("http://localhost:8000/users")
+      if(response){
+        console.log("users response",response)
+        setUsers(response?.data)
+      }
+    } catch (error) {
+      console.log(error)
+    } 
    }
-   console.log("this is selected option",selectedOption)
-  console.log(selectedOption)
+  // const onCheckChange = (e)=>{
+  //   console.log(e.target.checked)
+  //   setChecked(e.target.checked)
+  // }
+    
+
+
+  //  const handleSelectChange = (e,index)=>{
+  //     console.log(e.target.value)
+  //  }
+  //  console.log("this is selected option",selectedOption)
+  // console.log(selectedOption)
 
 
   return (
     <div className="App">
+      <BasicSelect options={users}/>
       {/* basic input usage example */}
-      <BasicInput  value={inputValue} onChange={setInputValue} />
+      {/* <BasicInput  value={inputValue} onChange={setInputValue} />
       <BasicInput  value={inputValue} onChange={setInputValue} />
       <BasicSelect options={options} handleChange={handleSelectChange} values={selectedOption}/>
-      <BasicCheckbox checked={checked}  onChange={onCheckChange} label={"basic checkbox"}/>
+      <BasicCheckbox checked={checked}  onChange={onCheckChange} label={"basic checkbox"}/> */}
     </div>
   );
 }
